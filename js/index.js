b041ccaa6783fa909446dbda35bcba2e554256eb
Anngelooo <AngeloColmenares00@gmail.com>
2024-10-10 13:25:27 +0200

    add more sneakers and tag updates

--- js/index.js
+++ js/index.js
@@ -11,6 +11,7 @@ fetchApiJson().then(shoe =>{
         let id = shoe.shoes[index]["id"];
         let image = shoe.shoes[index]["image"];
         let price = shoe.shoes[index]["price"];
+        let color = shoe.shoes[index]["color"];
         let model = shoe.shoes[index]["model"];
         let material = shoe.shoes[index]["material"];
         let description = shoe.shoes[index]["description"];
@@ -23,13 +24,13 @@ fetchApiJson().then(shoe =>{
                 <img src="${image}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Precio: ${price}€</h5>
+                    <p class="card-text">Color: ${color}</p>
                     <p class="card-text">Modelo: ${model}</p>
-                    <p class="card-text">Modelo: ${material}</p>
+                    <p class="card-text">Material: ${material}</p>
                     <p class="card-text">${description}</p>
                     <p class="card-text">Categoría: ${category}</p>
                 </div>                
                 <div class="card-body">
-                    <a href="#" class="card-link">Detalles</a>
                     <a href="#" class="card-link">Añadir al Carrito</a>
                 </div>
         </div>
